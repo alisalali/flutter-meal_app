2ab39d3e3b4cdf2d82889babbbe445c34d6eaaaf
alisalali <alis.alali@hotmail.com>
2023-10-09 08:20:02 +0300

    make widget tappable with inkwell

--- lib/widgets/category_grid_item.dart
+++ lib/widgets/category_grid_item.dart
@@ -7,25 +7,32 @@ class CategoryGridItem extends StatelessWidget {
   final Category category;
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(16),
-      decoration: BoxDecoration(
-          gradient: LinearGradient(
-        colors: [
-          category.color.withOpacity(0.55),
-          category.color.withOpacity(0.9),
-        ],
-        begin: Alignment.topLeft,
-        end: Alignment.bottomRight,
-      )),
-      child: Text(
-        category.title,
-        // apply title large theme with extra settings
-        style: Theme.of(context).textTheme.titleLarge!.copyWith(
-              color: Theme.of(context)
-                  .colorScheme
-                  .onBackground, // to access flutter color scheme (seedColor)
-            ),
+    return InkWell(
+      // to make widget tap able also u can use GestureDetector but there is no effect
+      onTap: () {},
+      splashColor: Theme.of(context).primaryColor, // make visual effect
+      borderRadius: BorderRadius.circular(16),
+      child: Container(
+        padding: const EdgeInsets.all(16),
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(16),
+            gradient: LinearGradient(
+              colors: [
+                category.color.withOpacity(0.55),
+                category.color.withOpacity(0.9),
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            )),
+        child: Text(
+          category.title,
+          // apply title large theme with extra settings
+          style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                color: Theme.of(context)
+                    .colorScheme
+                    .onBackground, // to access flutter color scheme (seedColor)
+              ),
+        ),
       ),
     );
   }
